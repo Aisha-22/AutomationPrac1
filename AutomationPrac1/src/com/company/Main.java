55d9f6f4f3bcff9040555186fa577ce23ce36847
Aisha-22 <17Khanyi03>
2020-10-21 18:39:25 +0200

    The basics of Java and Selenium

---
+++ AutomationPrac1/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import com.company.GreenKart;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+//        System.out.println("Aisha");
+
+        GreenKart greenkart = new GreenKart();
+
+        greenkart.veggieTales();
+    }
+}
